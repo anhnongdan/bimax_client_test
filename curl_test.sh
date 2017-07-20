e50d933f4e1c74553dfe584ea06fbe5e413b9af0
Vu Tran <baysao@gmail.com>
2017-07-20 21:54:12 +0700

    fix path

--- curl_test.sh
+++ curl_test.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-log='client_curl.log'
+log=$PWD/client_curl.log
 
 #this take 2 argument
 # instance number of bimax
 # and domain
 
 now=`date +%H_%M`
-curl https://${2}/pw$1/index.php > pw${1}_${now}.html
+archive=$PWD/pw${1}_${now}.html
+curl https://${2}/pw$1/index.php > $archive
 
 line=`cat pw${1}_${now}.html | wc -l`
 
