f093c404c6a513f05eb4ff71542cfe6d015598ba
Thang Nguyen <thangnt.az@gmail.com>
2017-07-20 18:09:24 +0700

    simple, just count the lines of curl response

---
+++ curl_test.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+log='client_curl.log'
+
+#this take 2 argument
+# instance number of bimax
+# and domain
+
+now=`date +%H_%M`
+curl https://${2}/pw$1/index.php > pw${1}_${now}.html
+
+line=`cat pw${1}_${now}.html | wc -l`
+
+if [ $line -lt 260 ]; then
+	cat '[${now}] pw${1}  $line respond' >> $log
+	exit 1
+fi
+rm -f pw${1}_${now}.html
+exit 0
+ 
+
